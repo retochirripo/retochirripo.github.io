766cb24d76b97c90781aa2ec07f14c2d82d7985d
Jou6778 <joseth64@outlook.es>
2024-10-07 10:38:22 -0600

    Update scriptHistorias.js

--- js/scriptHistorias.js
+++ js/scriptHistorias.js
@@ -1,34 +1,3 @@
-// Inicializa los carruseles
-var dianaCarousel = $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    center: true,
-    margin: 24,
-    dots: true,
-    loop: true,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        992: {
-            items: 3
-        }
-    }
-});
-
-// Aquí puedes inicializar otros carruseles de la misma forma
-var jaimeCarousel = $("#jaimeCarousel").owlCarousel({
-    // Configuración del carrusel de Jaime
-});
-var yuliCarousel = $("#yuliCarousel").owlCarousel({
-    // Configuración del carrusel de Yuli
-});
-
-// Función para abrir el modal
 function openModal(title, content, person) {
     document.getElementById("modalTitle").innerText = title; // Actualiza el título del modal
     document.getElementById("modalContent").innerText = content; // Actualiza el contenido del modal
@@ -41,13 +10,13 @@ function openModal(title, content, person) {
     // Muestra el carrusel correspondiente y reinicia
     if (person === 'Diana Carolina Segura Bolanos') {
         document.getElementById("dianaCarousel").style.display = "block";
-        dianaCarousel.trigger('to.owl.carousel', 0); // Reinicia el carrusel de Diana
+        $(".testimonial-carousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Diana
     } else if (person === 'Jaime Mendez Porras') {
         document.getElementById("jaimeCarousel").style.display = "block";
-        jaimeCarousel.trigger('to.owl.carousel', 0); // Reinicia el carrusel de Jaime
+        $("#jaimeCarousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Jaime
     } else if (person === 'Yuliana Valverde Alfaro') {
         document.getElementById("yuliCarousel").style.display = "block";
-        yuliCarousel.trigger('to.owl.carousel', 0); // Reinicia el carrusel de Yuli
+        $("#yuliCarousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Yuli
     }
 
     // Muestra el modal
