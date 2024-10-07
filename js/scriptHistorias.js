7c5b8a556b1646b3420834c45808ce27527bf643
Jou6778 <joseth64@outlook.es>
2024-10-07 10:40:49 -0600

    Update scriptHistorias.js

--- js/scriptHistorias.js
+++ js/scriptHistorias.js
@@ -1,24 +1,21 @@
-function openModal(title, content, person) {
-    document.getElementById("modalTitle").innerText = title; // Actualiza el título del modal
-    document.getElementById("modalContent").innerText = content; // Actualiza el contenido del modal
+  function openModal(title, content, person) {
+            document.getElementById("modalTitle").innerText = title; // Actualiza el título del modal
+            document.getElementById("modalContent").innerText = content; // Actualiza el contenido del modal
 
-    // Oculta todos los carruseles
-    document.getElementById("dianaCarousel").style.display = "none";
-    document.getElementById("jaimeCarousel").style.display = "none";
-    document.getElementById("yuliCarousel").style.display = "none";
+            // Oculta todos los carruseles
+            document.getElementById("dianaCarousel").style.display = "none";
+            document.getElementById("jaimeCarousel").style.display = "none";
+            document.getElementById("yuliCarousel").style.display = "none";
 
-    // Muestra el carrusel correspondiente y reinicia
-    if (person === 'Diana Carolina Segura Bolanos') {
-        document.getElementById("dianaCarousel").style.display = "block";
-        $(".testimonial-carousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Diana
-    } else if (person === 'Jaime Mendez Porras') {
-        document.getElementById("jaimeCarousel").style.display = "block";
-        $("#jaimeCarousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Jaime
-    } else if (person === 'Yuliana Valverde Alfaro') {
-        document.getElementById("yuliCarousel").style.display = "block";
-        $("#yuliCarousel").trigger('to.owl.carousel', 0); // Reinicia el carrusel de Yuli
-    }
+            // Muestra el carrusel correspondiente
+            if (person === 'Diana Carolina Segura Bolanos') {
+                document.getElementById("dianaCarousel").style.display = "block";
+            } else if (person === 'Jaime Mendez Porras') {
+                document.getElementById("jaimeCarousel").style.display = "block";
+            } else if (person === 'Yuliana Valverde Alfaro') {
+                document.getElementById("yuliCarousel").style.display = "block";
+            }
 
-    // Muestra el modal
-    $('#myModal').modal('show'); // Usa jQuery para mostrar el modal
-}
+            // Muestra el modal
+            $('#myModal').modal('show'); // Usa jQuery para mostrar el modal
+        }
