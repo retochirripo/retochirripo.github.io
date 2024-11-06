abf5a9f57df1091c3fd6f275b62c6a6a5731fb96
Jou6778 <joseth64@outlook.es>
2024-11-05 23:16:32 -0600

    Update scriptHistorias.js

--- js/scriptHistorias.js
+++ js/scriptHistorias.js
@@ -35,7 +35,7 @@ function openModal(title, content, person) {
     document.getElementById("modalTitle").innerText = title; 
     document.getElementById("modalContent").innerText = content; 
 
-    const carouselIds = ["daneyCarousel", "diegoCarousel", "jaimeCarousel", "oscarCarousel","joseCarousel","javierCarousel"]; 
+    const carouselIds = ["daneyCarousel", "diegoCarousel", "jaimeCarousel", "oscarCarousel", "joseCarousel", "javierCarousel"]; 
 
     // Oculta todos los carruseles y reinicia su estado
     carouselIds.forEach(id => {
